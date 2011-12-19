397fca0eb12b6b53f30d6953d5d2d27df0da31a6
Diego <diego.cabrera.mla@gmail.com>
2011-12-19 20:26:18 -0300

    Commit inicial

---
+++ src/com/melipago/views/HomeView.java
@@ -0,0 +1,22 @@
+package com.melipago.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class HomeView extends ViewGroup {
+
+	public HomeView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
