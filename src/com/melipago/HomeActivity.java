2936cf771811a6ef7162658f1023da4100b6f881
Diego <diego.cabrera.mla@gmail.com>
2012-01-02 19:35:18 -0300

    nada

--- src/com/melipago/HomeActivity.java
+++ src/com/melipago/HomeActivity.java
@@ -8,15 +8,7 @@ public class HomeActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
-      
-        
-        
         setContentView(R.layout.home);
     }
-    
-    
-    
     
 }
